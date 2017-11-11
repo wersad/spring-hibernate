8cc678ee3a9acb0a29d13028cdcd56d14f18cd85
Marcin Trościańczyk <Marcin.Troscianczyk@agora.pl>
2017-11-11 17:04:42 +0100

    disable data loading from file + persist entity by entity manager

--- src/main/java/pl/coderslab/main/Main.java
+++ src/main/java/pl/coderslab/main/Main.java
@@ -2,7 +2,10 @@ package pl.coderslab.main;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import pl.coderslab.entity.Book;
 
 public class Main {
 
@@ -14,7 +17,29 @@ public class Main {
 		
 		System.out.println("\n\nEntityManager was successfully created " + entityManager + "\n\n");
 		
-		entityManager.close();
-		entityManagerFactory.close();
+		Book book = new Book("Tytul nie zaladowany ze skryptu", "Autor nie zaladowany ze skryptu", "Opis nie zaladowany ze skryptu");
+		
+		EntityTransaction transaction = null;
+		
+		try {
+			
+			transaction = entityManager.getTransaction();
+		
+			transaction.begin();
+		
+			entityManager.persist(book);
+			entityManager.flush();
+			
+			transaction.commit();
+	
+		} catch(Exception e) {
+			
+			transaction.rollback();
+			e.printStackTrace();
+		} finally {
+			
+			entityManager.close();
+			entityManagerFactory.close();
+		}
 	}
 }
