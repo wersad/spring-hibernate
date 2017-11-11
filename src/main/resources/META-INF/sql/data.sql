dddc370484ce5013a35dce4741ef756e8f986f2b
Marcin Trościańczyk <Marcin.Troscianczyk@agora.pl>
2017-11-11 15:44:22 +0100

    execute sql script to load data to database

---
+++ src/main/resources/META-INF/sql/data.sql
@@ -0,0 +1,3 @@
+insert into book(title, description, author) values("Tytul 1", "Opis 1", "Autor 1")
+insert into book(title, description, author) values("Tytul 2", "Opis 2", "Autor 2")
+insert into book(title, description, author) values("Tytul 3", "Opis 3", "Autor 3")
